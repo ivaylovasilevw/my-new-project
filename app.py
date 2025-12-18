381a1cf55da98d1e70d594ac7763a42913f30590
ivaylovasilevw <ivaylo.vasilev30@gmail.com>
2025-12-18 13:08:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ st.title("Моето първо Streamlit приложение")
 name = st.text_input("Как се казваш?")
 if name:
       st.write(f"Здравей, {name}!")
+
+number = st.number_input("Въведете ва;ите години:")
+
+st.write(f"Вие сте на: {number}")
